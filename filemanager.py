8d0f068659845c335646a9e8d0a937f780cb6bf8
MurVladimirOS <ekliiehxmeh@gmail.com>
2022-09-26 21:50:22 +0300

    fix: фadded the output fintion type

--- filemanager.py
+++ filemanager.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 
-def read_file(path: str, header: int = None):
+def read_file(path: str, header: int = None) -> pd.DataFrame:
     excel_data = pd.read_excel(path, header=header)
     return excel_data
