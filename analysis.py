f8440d205e64f7fe7bbad841505c555c8278038a
MurVladimirOS <ekliiehxmeh@gmail.com>
2022-10-18 01:04:02 +0300

    feat: calculation of abs analysis has been rewritten without the help of pandass

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
+from functools import reduce
+
 import pandas as pd
-from pydantic import BaseModel
+from typing import Dict, Union, List
 from models import (
     AnalysisModel,
     ABCModels,
@@ -35,12 +37,12 @@ def analysis(type_analysis: str, path: str) -> pd.DataFrame:
 def abc(path: str) -> pd.DataFrame:
     """Делает расчет abc анализа"""
     # path = "./tests/data/abc_test.xlsx"
-    data = read_file(path=path)
-    data = data.sort_values(by=[ABCModels.DATA_ANALYSIS], ascending=False)
-    data = share(data=data)
-    data = accumulated_share(data=data)
-    data = category(data=data)
-    data = data.round(2)
+    # data = read_file(path=path)
+    # data = data.sort_values(by=[ABCModels.DATA_ANALYSIS], ascending=False)
+    # data = share(data=data)
+    # data = accumulated_share(data=data)
+    # data = category(data=data)
+    # data = data.round(2)
     validators_abc(data=data)
     data = data.reset_index(drop=True)
     return data
@@ -51,25 +53,91 @@ def abc(path: str) -> pd.DataFrame:
 #     return result
 
 
-def share(data: pd.DataFrame) -> pd.DataFrame:
-    """Cчитает долю от анализируемой колонки в %"""
-    data[ABCModels.SHARE] = data[ABCModels.DATA_ANALYSIS] / (data[ABCModels.DATA_ANALYSIS].sum()) * 100
-    return data
+# def share(data: pd.DataFrame) -> pd.DataFrame:
+#     """Cчитает долю от анализируемой колонки в %"""
+#     data[ABCModels.SHARE] = data[ABCModels.DATA_ANALYSIS] / (data[ABCModels.DATA_ANALYSIS].sum()) * 100
+#     return data
 
 
-def accumulated_share(data: pd.DataFrame) -> pd.DataFrame:
-    """Считает накопленную долю в %"""
-    data[ABCModels.ACCUMULATED_SHARE] = data[ABCModels.SHARE].cumsum()
-    return data
+# def accumulated_share(data: pd.DataFrame) -> pd.DataFrame:
+#     """Считает накопленную долю в %"""
+#     data[ABCModels.ACCUMULATED_SHARE] = data[ABCModels.SHARE].cumsum()
+#     return data
 
 
-def category(data: pd.DataFrame) -> pd.DataFrame:
-    """Разбивает по категориям a,b,c в зависимости от накопленной доли"""
-    data.loc[data[ABCModels.ACCUMULATED_SHARE] < 80, ABCModels.CATEGORY] = 'A'
-    data.loc[(data[ABCModels.ACCUMULATED_SHARE] >= 80) & (data[ABCModels.ACCUMULATED_SHARE] < 95),
-             ABCModels.CATEGORY] = 'B'
-    data.loc[data[ABCModels.ACCUMULATED_SHARE] >= 95, ABCModels.CATEGORY] = 'C'
-    return data
+# def category(data: pd.DataFrame) -> pd.DataFrame:
+#     """Разбивает по категориям a,b,c в зависимости от накопленной доли"""
+#     data.loc[data[ABCModels.ACCUMULATED_SHARE] < 80, ABCModels.CATEGORY] = 'A'
+#     data.loc[(data[ABCModels.ACCUMULATED_SHARE] >= 80) & (data[ABCModels.ACCUMULATED_SHARE] < 95),
+#              ABCModels.CATEGORY] = 'B'
+#     data.loc[data[ABCModels.ACCUMULATED_SHARE] >= 95, ABCModels.CATEGORY] = 'C'
+#     return data
 
 
+class ABCAnalysis:
+    """ABC analysis."""
+    def __init__(self, data: Dict[str, List[Union[int, str, float]]]):
+        self.data = data
 
+    def result(self):
+        return self.data
+
+    @property
+    def sorted(self):
+        values: List[Union[int, float]] = self.data[ABCModels.DATA_ANALYSIS].copy()
+        values.sort(reverse=True)
+        result = {}
+        for key in self.data.keys():
+            if key == ABCModels.DATA_ANALYSIS:
+                result[key] = values
+                continue
+            arr = []
+            for value in values:
+                index = self.data[ABCModels.DATA_ANALYSIS.value].index(value)
+                arr.append(self.data[key][index])
+            result[key] = arr
+        self.data = result
+
+    @property
+    def share(self):
+        """Cчитает долю от анализируемой колонки в %"""
+        sum_analysis_numbers: Union[int, float] = sum(self.data[ABCModels.DATA_ANALYSIS.value])
+        arr: List[Union[int, float]] = []
+        for analysis_number in self.data[ABCModels.DATA_ANALYSIS.value]:
+            s = analysis_number / sum_analysis_numbers * 100
+            arr.append(s)
+        self.data[ABCModels.SHARE.value] = arr
+
+    @property
+    def accumulated_share(self):
+        """Считает накопленную долю в %"""
+        arr: List[Union[int, float]] = []
+        accum_numbers = 0
+        for number_share in self.data[ABCModels.SHARE.value]:
+            accum_numbers += number_share
+            arr.append(accum_numbers)
+        self.data[ABCModels.ACCUMULATED_SHARE.value] = arr
+
+    @property
+    def category(self):
+        """Разбивает по категориям a,b,c в зависимости от накопленной доли"""
+        arr: List[str] = []
+        for accum_numb in self.data[ABCModels.ACCUMULATED_SHARE.value]:
+            if accum_numb < 80:
+                arr.append("A")
+            elif 80 <= accum_numb < 95:
+                arr.append("B")
+            elif accum_numb >= 95:
+                arr.append("C")
+        self.data[ABCModels.CATEGORY.value] = arr
+
+    @property
+    def round(self):
+        arr: List[Union[int, float]] = []
+        arr2: List[Union[int, float]] = []
+        for value_share in self.data[ABCModels.SHARE.value]:
+            arr.append(round(value_share, 2))
+        self.data[ABCModels.SHARE.value] = arr
+        for value_share in self.data[ABCModels.ACCUMULATED_SHARE.value]:
+            arr2.append(round(value_share, 2))
+        self.data[ABCModels.ACCUMULATED_SHARE.value] = arr2
--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -4,6 +4,7 @@ from analysis import (
     analysis,
     NotFoundAnalysisError,
     abc,
+    ABCAnalysis
 )
 from code_errors import (
     FileNotNumericValueError,
@@ -13,7 +14,41 @@ from code_errors import (
     CategoryIncorrectError,
 )
 from validators import validators_abc
+from models import ABCModels
 
+
+@pytest.fixture
+def data_result():
+    return {
+        'код идентификатор PLU': [1, 2, 4, 3, 5],
+        'наименование анализируемых позиций': [
+            'Товар 1',
+            'Товар 2',
+            'Товар 4',
+            'Товар 3',
+            'Товар 5'],
+        'данные по анализируемому критерию (продажи/оборот/прибыль)': [
+            100, 50, 20, 8, 5
+        ],
+        'Доля': [54.64, 27.32, 10.93, 4.37, 2.73],
+        'Аккум.доля': [54.64, 81.97, 92.9, 97.27, 100.0],
+        'Категория': ['A', 'B', 'B', 'C', 'C'],
+    }
+
+@pytest.fixture
+def data_input():
+    return {
+        'код идентификатор PLU': [1, 2, 3, 4, 5],
+        'наименование анализируемых позиций': [
+            'Товар 1',
+            'Товар 2',
+            'Товар 3',
+            'Товар 4',
+            'Товар 5'],
+        'данные по анализируемому критерию (продажи/оборот/прибыль)': [
+            100, 50, 8, 20, 5
+        ],
+    }
 
 class TestGroup:
     def test_not_numeric_value_in_file(self):
@@ -60,3 +95,47 @@ class TestGroup:
     def test_not_found_analysis(self):
         with pytest.raises(NotFoundAnalysisError):
             analysis(type_analysis='XXX', path="./tests/data/abc_test.xlsx")
+
+
+def test_sorted(data_input, data_result):
+    a = ABCAnalysis(data=data_input)
+    a.sorted
+    result = a.result()
+    assert result[ABCModels.CODE_PLU] == data_result[ABCModels.CODE_PLU]
+    assert result[ABCModels.NAME_ANALYSIS_POSITIONS] == data_result[ABCModels.NAME_ANALYSIS_POSITIONS]
+    assert result[ABCModels.DATA_ANALYSIS] == data_result[ABCModels.DATA_ANALYSIS]
+
+
+def test_share(data_input, data_result):
+    a = ABCAnalysis(data=data_input)
+    a.sorted
+    a.share
+    result = a.result()
+    assert result[ABCModels.SHARE] == [
+        54.644808743169406,
+        27.322404371584703,
+        10.92896174863388,
+        4.371584699453552,
+        2.73224043715847
+    ]
+
+
+def test_accum_share(data_input, data_result):
+    a = ABCAnalysis(data=data_input)
+    a.sorted
+    a.share
+    a.accumulated_share
+    a.round
+    result = a.result()
+    assert result[ABCModels.ACCUMULATED_SHARE] == data_result[ABCModels.ACCUMULATED_SHARE]
+
+
+def test_class_abc(data_input, data_result):
+    a = ABCAnalysis(data=data_input)
+    a.sorted
+    a.share
+    a.accumulated_share
+    a.category
+    a.round
+    result = a.result()
+    assert result == data_result
