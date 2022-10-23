045d9794dfd89bac2261eabf4db89c3a0b133b9c
MurVladimirOS <ekliiehxmeh@gmail.com>
2022-10-23 22:53:38 +0300

    feat: validate read_exel and add tests filemanager

--- filemanager.py
+++ filemanager.py
@@ -1,13 +1,44 @@
-import pandas as pd
-from typing import Optional
-from validators import validators_read_file
+import openpyxl
+from code_errors import FileIsEmptyError
+from typing import Dict, Union, List
 
 
-def read_file(path: str, header: Optional[int] = 0) -> pd.DataFrame:
-    data = pd.read_excel(path, header=header)
+def read_exel(path: str):
+    wb = openpyxl.load_workbook(path)
+    worksheet = wb.worksheets[0]
+    iter_rows = worksheet.iter_rows()
+
     try:
-        validators_read_file(data=data)
-    except Exception as err:
-        raise Exception(err)
-    return data
+        header: List[str] = [cell.value for cell in next(iter_rows)]
+    except StopIteration:
+        raise FileIsEmptyError
+    rows: List[Dict[str, List[Union[int, str, float]]]] = read_rows(header=header, rows=iter_rows)
+    if not rows:
+        raise FileIsEmptyError
+    result = convert_dict(header=header, rows=rows)
+    return result
 
+
+def convert_dict(header, rows):
+    """Конвертирует в один словарь"""
+    result: dict[str, List[Union[int, str, float]]] = {}
+    for key in header:
+        arr: List[Union[int, str, float]] = []
+        for into_rows in rows:
+            arr.append(into_rows[key])
+        result[key] = arr
+    return result
+
+
+def read_rows(header, rows):
+    result: List[Dict[str, List[Union[int, str, float]]]] = []
+    for row in rows:
+        result.append(read_cells(header, cells=row))
+    return result
+
+
+def read_cells(header, cells):
+    result: Dict[str, List[Union[int, str, float]]] = {}
+    for j, cell in enumerate(cells):
+        result[header[j]] = cell.value
+    return result
--- validators.py
+++ validators.py
@@ -1,33 +1,34 @@
-import numpy as np
-import pandas as pd
-from models import ABCModels
-from code_errors import (
-    FileNotNumericValueError,
-    FileNegativeValueError,
-    SumValuesIsNot100Error,
-    AccumulatedValuesNotCorrect,
-    CategoryIncorrectError,
-    FileIsEmptyError,
-    FileIsEmptyValueError,
-)
-
-
-def validators_abc(data: pd.DataFrame) -> None:
-    """Проводит валидацию на предмет ошибок"""
-    if not data[ABCModels.DATA_ANALYSIS].apply(np.isreal).all():
-        raise FileNotNumericValueError
-    if (data[ABCModels.DATA_ANALYSIS] < 0).any():
-        raise FileNegativeValueError
-    if data[ABCModels.SHARE].values.sum() <= 99.99:
-        raise SumValuesIsNot100Error
-    if not (99.99 < data[ABCModels.ACCUMULATED_SHARE].iloc[-1] < 100.01):
-        raise AccumulatedValuesNotCorrect
-    if not data[ABCModels.CATEGORY].isin([symbol for symbol in "ABC"]).all():
-        raise CategoryIncorrectError
-
-
-def validators_read_file(data: pd.DataFrame) -> None:
-    if data.empty:
-        raise FileIsEmptyError
-    if data.isnull().any().any():
-        raise FileIsEmptyValueError
+# import numpy as np
+# import pandas as pd
+#
+# from models import ABCModels
+# from code_errors import (
+#     FileNotNumericValueError,
+#     FileNegativeValueError,
+#     SumValuesIsNot100Error,
+#     AccumulatedValuesNotCorrect,
+#     CategoryIncorrectError,
+#     FileIsEmptyError,
+#     FileIsEmptyValueError,
+# )
+#
+#
+# def validators_abc(data: pd.DataFrame) -> None:
+#     """Проводит валидацию на предмет ошибок"""
+#     if not data[ABCModels.DATA_ANALYSIS].apply(np.isreal).all():
+#         raise FileNotNumericValueError
+#     if (data[ABCModels.DATA_ANALYSIS] < 0).any():
+#         raise FileNegativeValueError
+#     if data[ABCModels.SHARE].values.sum() <= 99.99:
+#         raise SumValuesIsNot100Error
+#     if not (99.99 < data[ABCModels.ACCUMULATED_SHARE].iloc[-1] < 100.01):
+#         raise AccumulatedValuesNotCorrect
+#     if not data[ABCModels.CATEGORY].isin([symbol for symbol in "ABC"]).all():
+#         raise CategoryIncorrectError
+#
+#
+# def validators_read_file(data: pd.DataFrame) -> None:
+#     if data.empty:
+#         raise FileIsEmptyError
+#     if data.isnull().any().any():
+#         raise FileIsEmptyValueError
